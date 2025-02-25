582a8a501d22b3af476a2a714eea8deed5a10a49
Sebastian <sebastian.krahmer@gmail.com>
2025-02-25 13:49:06 +0100

    - tensor-pwn: Adding more comments

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,17 @@
-# Sample hub file to exec commands
+# Sample hub file to exec commands (trivially).
+#
+# Trigger by
+#
+# model = torch.hub.load('stealth/tensor-pwn', '')
+#
+# Note that this does not require any Pickle data to be loaded.
+#
+# If the repo-name was something more trustworthy, torchhub
+# still depends solely on HTTPS/CA-bundle being trusted. This
+# is to doubt and why e.g. golang adds more layers of security when
+# fetching modules. It also doesn't matter whether in future the pytorch
+# unpickler only loads weights and avoids arbitrary commands in Pickle
+# files if you can just add python scripts to be executed to the repo.
 
 dependencies = []
 
